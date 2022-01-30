c90be8c8e5b164155fb2e4c1c3a9af2e69d67d97
cometbroom <alawi.wow@gmail.com>
2022-01-30 10:31:46 +0100

    Added all button functionalities

--- app.js
+++ app.js
@@ -28,6 +28,27 @@ document.addEventListener("DOMContentLoaded", () => {
         } else generate();
     }
 
+    function updateTiles(indexOld, tilesNew) {
+        squares[indexOld[0]].innerHTML = tilesNew[0];
+        squares[indexOld[1]].innerHTML = tilesNew[1];
+        squares[indexOld[2]].innerHTML = tilesNew[2];
+        squares[indexOld[3]].innerHTML = tilesNew[3];
+    }
+    function updateTile(indexOld, tileNew) {
+        squares[indexOld].innerHTML = tileNew;
+    }
+
+    function filteredAndZeros(tilesDir) {
+        let filteredTiles = tilesDir.filter((num) => num);
+        let missing = 4 - filteredTiles.length;
+        let zeros = Array(missing).fill(0);
+
+        return {
+            filtered: filteredTiles,
+            zeros: zeros,
+        };
+    }
+
     //get the rows by having the expression on the first tile only.
     function getRows() {
         let rows = [];
@@ -48,6 +69,25 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
         return rows;
+    }
+
+    function getCols() {
+        let cols = [];
+
+        for (let i = 0; i < 4; ++i) {
+            let valueOne = squares[i].innerHTML;
+            let valueTwo = squares[i + width].innerHTML;
+            let valueThree = squares[i + width * 2].innerHTML;
+            let valueFour = squares[i + width * 3].innerHTML;
+            let column = [
+                parseInt(valueOne),
+                parseInt(valueTwo),
+                parseInt(valueThree),
+                parseInt(valueFour),
+            ];
+            cols.push(column);
+        }
+        return cols;
     }
 
     //swipe right
@@ -83,17 +123,50 @@ document.addEventListener("DOMContentLoaded", () => {
             squares[pos + 3].innerHTML = newRow[3];
         }
     }
+    function moveDown() {
+        let cols = getCols();
+
+        for (let i = 0; i < cols.length; ++i) {
+            let fAndZ = filteredAndZeros(cols[i]);
+            let newColumn = fAndZ.zeros.concat(fAndZ.filtered);
+            updateTiles(
+                [i, i + width, i + width * 2, i + width * 3],
+                newColumn
+            );
+        }
+    }
+    function moveUp() {
+        let cols = getCols();
+
+        for (let i = 0; i < cols.length; ++i) {
+            let fAndZ = filteredAndZeros(cols[i]);
+            let newColumn = fAndZ.filtered.concat(fAndZ.zeros);
+            updateTiles(
+                [i, i + width, i + width * 2, i + width * 3],
+                newColumn
+            );
+        }
+    }
+
     function combineRow() {
         for (let i = 0; i < 15; ++i) {
-            let [leftSquare, rightSquare] = [
-                squares[i].innerHTML,
-                squares[i + 1].innerHTML,
-            ];
-            if (leftSquare === rightSquare) {
+            if (squares[i].innerHTML === squares[i + 1].innerHTML) {
                 let combinedTotal =
-                    parseInt(leftSquare) + parseInt(rightSquare);
-                squares[i].innerHTML = combinedTotal;
-                squares[i + 1].innerHTML = 0;
+                    parseInt(squares[i].innerHTML) +
+                    parseInt(squares[i + 1].innerHTML);
+                updateTile(i, combinedTotal);
+                updateTile(i + 1, 0);
+            }
+        }
+    }
+    function combineColumn() {
+        for (let i = 0; i < 12; ++i) {
+            if (squares[i].innerHTML === squares[i + width].innerHTML) {
+                let combinedTotal =
+                    parseInt(squares[i].innerHTML) +
+                    parseInt(squares[i + width].innerHTML);
+                updateTile(i, combinedTotal);
+                updateTile(i + width, 0);
             }
         }
     }
@@ -117,6 +190,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 break;
         }
     }
+
+    //Logic
+    //check for the number 2048
+
+
+    //Events
     document.addEventListener("keyup", contorl);
 
     function keyRight() {
@@ -129,6 +208,18 @@ document.addEventListener("DOMContentLoaded", () => {
         moveLeft();
         combineRow();
         moveLeft();
+        generate();
+    }
+    function keyUp() {
+        moveUp();
+        combineColumn();
+        moveUp();
+        generate();
+    }
+    function keyDown() {
+        moveDown();
+        combineColumn();
+        moveDown();
         generate();
     }
 });
